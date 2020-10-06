dc7ef8e5f57c4066a6a5da7e61073ef3f6a8f15e
hesham <17p6020@eng.asu.edu.eg>
2020-10-06 23:01:56 +0200

    HomeScreen

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagramredesign/Data/Posts.dart';
+import 'package:instagramredesign/Widgets/PostContainer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +9,162 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> buildStories() {
+    List<Widget> stories = new List<Widget>();
+    for (Post story in homePosts) {
+      stories.add(
+        Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [const Color(0xffe43e68), const Color(0xfffaa449)],
+            ),
+            borderRadius: BorderRadius.circular(35),
+          ),
+          margin: EdgeInsets.symmetric(horizontal: 5),
+          child: Center(
+            child: CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage(story.imgUrl),
+            ),
+          ),
+        ),
+      );
+    }
+    return stories;
+  }
+
+  List<Widget> buildPosts() {
+    List<Widget> posts = new List<Widget>();
+    for (Post post in homePosts) {
+      posts.add(PostContainer(
+        post: post,
+      ));
+    }
+    return posts;
+  }
+
+  int _currentTab = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Container(
+          height: 30,
+          child: Image.asset('assets/logo.png'),
+        ),
+        centerTitle: true,
+        leading: Container(
+          padding: EdgeInsets.all(10),
+          child: Image.asset('assets/igtv.png'),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Icon(
+              Icons.send,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 60,
+            padding: EdgeInsets.only(left: 20),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/pp.jpg'),
+                          )),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Icon(
+                        Icons.add_circle,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: buildStories(),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: buildPosts(),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Container(
+          width: 60,
+          height: 60,
+          child: Icon(
+            Icons.add,
+            size: 40,
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [const Color(0xffe43e68), const Color(0xfffaa449)],
+            ),
+          ),
+        ),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: Colors.white,
+        currentIndex: _currentTab,
+        onTap: (index) {
+          setState(() {
+            _currentTab = index;
+          });
+        },
+        activeColor: Color(0xffe43e68),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_alert),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/Widgets/PostContainer.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:instagramredesign/Data/Posts.dart';
+
+class PostContainer extends StatefulWidget {
+  final Post post;
+
+  const PostContainer({Key key, this.post}) : super(key: key);
+  @override
+  _PostContainerState createState() => _PostContainerState();
+}
+
+class _PostContainerState extends State<PostContainer> {
+  final Shader linearGradient = LinearGradient(
+    colors: <Color>[Color(0xffe43e68), Color(0xfffaa449)],
+  ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 15,
+                  backgroundImage: AssetImage(widget.post.imgUrl),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  widget.post.userName,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    foreground: Paint()..shader = linearGradient,
+                  ),
+                )
+              ],
+            ),
+            Icon(
+              Icons.more_horiz,
+              color: Colors.grey,
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 270,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(widget.post.imgUrl),
+              )),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 30,
+                  child: Image.asset('assets/heart.png'),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  widget.post.likes,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 30,
+                  child: Icon(Icons.comment),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  widget.post.comments,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 30,
+                  child: Icon(Icons.save),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  widget.post.saves,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
